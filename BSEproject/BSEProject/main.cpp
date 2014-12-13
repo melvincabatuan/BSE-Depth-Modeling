c4abead14a6799683975c68370ff4117b3f4d17e
janthony10 <john.anthony.jose2005@gmail.com>
2014-12-14 03:04:15 +0800

    Initial Commit

---
+++ BSEproject/BSEProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BSEProject
+//
+//  Created by John Anthony Jose on 12/14/14.
+//  Copyright (c) 2014 Anthony. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
